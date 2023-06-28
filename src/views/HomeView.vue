a3fd47de8515002814897e5eb64f537abc41e229
AnisaFitri03 <12150120006@students.uin-suska.ac.id>
2023-06-28 08:15:31 +0700

    commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+
+<div class="halaman">
+
+</div>
+  <h1 src="">Perpustakaan</h1>
+  <img src="../assets/nisa.jpg" class="rounded mx-auto d-block" alt="...">
+
+
+</template>
+
+<style>
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 10px;
+  width:1100px;
+  height:460px;
+
+}
+
+h1{
+  text-align: center;
+  font-style: italic;
+  font-size: 35px;
+  color: #323232;
+}
+
+</style>
